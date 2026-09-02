fd672cff6a15cc05152a28214c1a105387365f3d
Sameer Singh <sam@sams-MacBook-Pro.local>
2026-09-02 16:43:22 +0530

    Initial commit

--- app/app.py
+++ app/app.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/hello-world")
-def hello_world():
-    return {"message": "Hello, World!"} 
+text_posts={1: {"title": "New Post", "content": "cool text post"}}
+
+@app.get("/posts")
+def get_all_posts():
+    return text_posts
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    return text_posts.get(id)
